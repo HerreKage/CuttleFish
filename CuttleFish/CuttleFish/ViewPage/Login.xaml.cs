0c7970bbb1736b2732f66e0a376adb513ff375a1
Ahsan Javaid Iqbal <ahsa0005@edu.easj.dk>
2018-05-04 10:22:56 +0200

    Login Page Done

--- CuttleFish/CuttleFish/ViewPage/Login.xaml.cs
+++ CuttleFish/CuttleFish/ViewPage/Login.xaml.cs
@@ -25,6 +25,31 @@ namespace CuttleFish.ViewPage
         public Login()
         {
             this.InitializeComponent();
+
+            int change = 1;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(4);
+            timer.Tick += (o, a) =>
+            {
+                // If we'd go out of bounds then reverse
+                int newIndex = LoginFw.SelectedIndex + change;
+                if (LoginFw.Items != null && (newIndex >= LoginFw.Items.Count || newIndex < 0))
+                {
+                    change *= -1;
+                }
+
+                LoginFw.SelectedIndex += change;
+            };
+
+            timer.Start();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Startside));
         }
     }
 }
